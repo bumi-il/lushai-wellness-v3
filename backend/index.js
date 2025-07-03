8071b8c64f477117595e43fb0dba0a44fbec9c81
bumi-il <mg0552974085@gmail.com>
2025-07-03 22:49:22 +0300

    backend env

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { config } from 'dotenv';
+
+config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('Server listening on port ' + PORT));
