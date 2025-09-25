95870878ed171892f7db264e2dd319e022506561
bumi-il <mg0552974085@gmail.com>
2025-09-25 23:07:53 +0300

    update

--- frontend/src/components/InfoCard.jsx
+++ frontend/src/components/InfoCard.jsx
@@ -3,6 +3,23 @@ import './css/InfoCard.css';
 const InfoCard = ({ info }) => {
     const { Icon, title, description, link, color } = info;
 
+    if (!link) {
+        return (
+            <div className='info-card'>
+                <div
+                    className='info-card-icon'
+                    style={{ backgroundColor: color }}
+                >
+                    <Icon size={25} />
+                </div>
+                <div className='info-card-info'>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <a href={link}>
             <div className='info-card'>
